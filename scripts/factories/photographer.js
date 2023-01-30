6ba84598dd5cdcd96a99f9412864ff0ca7281914
ZhannaZucher <jzucher@gmail.com>
2023-01-30 20:32:21 +0100

    first commit

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,16 +1,46 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, id, tagline, price} = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        const article = document.createElement('article');
+
+        const link = document.createElement('a');
+        link.setAttribute("href", `../../photographer.html?id=${id}`);
+        link.setAttribute("role", "link");
+        link.setAttribute("title", `Visiter la page de profil de ${name}`);
+
+        const img = document.createElement('img');
+        img.setAttribute("src", picture);
+        img.setAttribute("alt", "");
+
+        const h2 = document.createElement('h2');
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+
+        const divDescription = document.createElement('div');
+
+        const location = document.createElement('p');
+        location.textContent = `${city}, ${country}`;
+        location.classList.add("photographer__location");
+
+        const slogan = document.createElement('p');
+        slogan.textContent = tagline;
+        slogan.classList.add("photographer__slogan");
+
+        const pricePerDay = document.createElement('p');
+        pricePerDay.textContent = `${price}€/jour`;
+        pricePerDay.classList.add("photographer__price");
+
+        article.appendChild(link);
+        link.appendChild(img);
+        link.appendChild(h2);
+        article.appendChild(divDescription);
+        divDescription.appendChild(location);
+        divDescription.appendChild(slogan);
+        divDescription.appendChild(pricePerDay);
+
+
         return (article);
     }
     return { name, picture, getUserCardDOM }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,4 +1,10 @@
-    async function getPhotographers() {
+//Récupération des data depuis le fichier JSON
+async function getPhotographers() {
+    const response = await fetch('../data/photographers.json');
+    return await response.json();
+}
+    
+   /* async function getPhotographers() {
         // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
         // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
         let photographers = [
@@ -9,7 +15,7 @@
                 "country": "France",
                 "tagline": "Ceci est ma data test",
                 "price": 400,
-                "portrait": "account.png"
+                "portrait": "../photographers/account.png"
             },
             {
                 "name": "Autre data test",
@@ -18,16 +24,16 @@
                 "country": "UK",
                 "tagline": "Ceci est ma data test 2",
                 "price": 500,
-                "portrait": "account.png"
+                "portrait": "../photographers/account.png"
             },
         ]
         // et bien retourner le tableau photographers seulement une fois récupéré
         return ({
             photographers: [...photographers, ...photographers, ...photographers]})
-    }
+    }*/
 
     async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
+        const photographersSection = document.querySelector(".photographer-section");
 
         photographers.forEach((photographer) => {
             const photographerModel = photographerFactory(photographer);
@@ -38,7 +44,7 @@
 
     async function init() {
         // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
+        const { photographers } = await getPhotographers(); //pourquoi avec {}?
         displayData(photographers);
     };
     
