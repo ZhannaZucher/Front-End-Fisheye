e15e6702838f4631f38840c6409417040dd16a05
ZhannaZucher <jzucher@gmail.com>
2023-02-01 21:05:38 +0100

    fix : remove old code base

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,37 +1,9 @@
-//Récupération des data depuis le fichier JSON
+//Récupération des données des photographes depuis le fichier JSON
 async function getPhotographers() {
     const response = await fetch('data/photographers.json');
     return await response.json();
 }
     
-   /* async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "../photographers/account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "../photographers/account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
-    }*/
-
     async function displayData(photographers) {
         const photographersSection = document.querySelector(".photographer-section");
 
@@ -44,7 +16,7 @@ async function getPhotographers() {
 
     async function init() {
         // Récupère les datas des photographes
-        const { photographers } = await getPhotographers(); //pourquoi avec {}?
+        const { photographers } = await getPhotographers();
         displayData(photographers);
     };
     
