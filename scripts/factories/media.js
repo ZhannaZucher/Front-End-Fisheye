dcf8dfdde70bc81ee0fd361569f25d90a2f83c22
ZhannaZucher <jzucher@gmail.com>
2023-02-08 10:49:03 +0100

    add : media id attr, remove todo list

--- scripts/factories/media.js
+++ scripts/factories/media.js
@@ -1,10 +1,3 @@
-/*A FAIRE:
-1. fonction factory pour DOM images
-2. fonction factory pour DOM videos
-3. fonction qui regroupe les deux en fonction du type de media
-importer dans photograph.js pour affichage
-*/
-
 function mediaFactory(data) {
 
 	const { id, photographerId, title, image, video, date, price, likes } = data;
@@ -29,6 +22,7 @@ function mediaFactory(data) {
 
 		const article = document.createElement('article');
 		article.classList.add("media");
+		article.setAttribute("id", `media-${id}`);
 
 		const link = document.createElement('a');
 		link.classList.add("media__link");
@@ -68,96 +62,4 @@ function mediaFactory(data) {
 		return (article);
 	}
 	return { getViewCardDOM };
-}
-
-/*function mediaFactory(data) {
-	const { id, photographerId, title, image, video, date, price, likes } = data;
-	let mediaTemplate = "";
-
-   if (data.hasOwnProperty("image")) {
-		
-		function getViewCardDOM() {
-			const article = document.createElement('article');
-			article.classList.add("media");
-
-			const link = document.createElement('a');
-			link.classList.add("media__link");
-			link.setAttribute("href", "");
-
-			const img = document.createElement('img');
-			img.setAttribute("src", `assets/medias/${photographerId}/${image}`);
-			img.setAttribute("alt", `${title}`);
-
-			const aboutSection = document.createElement("div");
-			aboutSection.classList.add("media__about");
-
-			const mediaTitle = document.createElement("p");
-			mediaTitle.textContent = title;
-
-			const likesDiv = document.createElement("div");
-			likesDiv.classList.add("media__likes");
-			
-			const likesQuantity = document.createElement("p");
-			likesQuantity.classList.add("media__rating");
-			likesQuantity.textContent = `${likes}`;
-
-			const addLikesButton = document.createElement("button");
-
-			const heartIcon = createElement('img');
-
-			article.appendChild(link);
-			link.appendChild(img);
-			article.appendChild(aboutSection);
-			aboutSection.appendChild(mediaTitle);
-			aboutSection.appendChild(likesDiv);
-			likesDiv.appendChild(likesQuantity);
-			likesDiv.appendChild(addLikesButton);
-			addLikesButton.appendChild(heartIcon);
-
-			mediaTemplate += article;
-
-
-		} else if (data.hasOwnProperty("video")) {
-			const article = document.createElement('article');
-			article.classList.add("media");
-
-			const link = document.createElement('a');
-			link.classList.add("media__link");
-			link.setAttribute("href", "");
-
-			const img = document.createElement('img');
-			img.setAttribute("src", `assets/medias/${photographerId}/${image}`);
-			img.setAttribute("alt", `${title}`);
-
-			const aboutSection = document.createElement("div");
-			aboutSection.classList.add("media__about");
-
-			const mediaTitle = document.createElement("p");
-			mediaTitle.textContent = title;
-
-			const likesDiv = document.createElement("div");
-			likesDiv.classList.add("media__likes");
-
-			const likesQuantity = document.createElement("p");
-			likesQuantity.classList.add("media__rating");
-			likesQuantity.textContent = `${likes}`;
-
-			const addLikesButton = document.createElement("button");
-
-			const heartIcon = createElement('img');
-
-			article.appendChild(link);
-			link.appendChild(video);
-			article.appendChild(aboutSection);
-			aboutSection.appendChild(mediaTitle);
-			aboutSection.appendChild(likesDiv);
-			likesDiv.appendChild(likesQuantity);
-			likesDiv.appendChild(addLikesButton);
-			addLikesButton.appendChild(heartIcon);
-
-			mediaTemplate += article;
-		}
-		return (mediaTemplate);
-	}
-	return { getImageViewDOM };
-}*/
+};
