2f0c840c5c3f61fcf8b1d7f069c90f5e600f4bea
ZhannaZucher <jzucher@gmail.com>
2023-02-16 11:00:47 +0100

    clean : code + comments

--- scripts/utils/lightbox.js
+++ scripts/utils/lightbox.js
@@ -2,22 +2,21 @@
 TODO list:
 1. fonction initLightbox avec les listeners  pour:
 	OK! ouvrir LBOX (fonction à créer) + gestion des aria
-   	fermer LBOX (fonction à créer) + gestion des aria
-		goToNext (fonction à créer)
-		goToPrevious (fonction à créer) 
-		events keybord
-2. fonction "ouvrir LBOX" avec
-		Lbox style block + idem pour lBOX bcgrd
+   	OK! fermer LBOX (fonction à créer) + gestion des aria
+		=> goToNext (fonction à créer)
+		=> goToPrevious (fonction à créer) 
+		=> events keybord (arrows)
+2. OK! fonction "ouvrir LBOX" avec
+		Lbox style block 
 		main : aria hidden=true, LBOX wrapper = false
 		focus sur btn close
 		appel de la fonction setMediaView (soit avec links soit avec id)
-3. OK! fonction fermer LBOX:
-		display none + aria + focus
-4. fonction qui gère next/prev slideShow (avec index)
+3. OK! fonction fermer LBOX: display none + aria + focus
+4.  => fonction qui gère next/prev slideShow (avec index)
 ...
 */
 //Récupération des éléments DOM
-const lightbox = document.querySelector(".lightbox-wrapper");//a mettre dans display
+const lightbox = document.querySelector(".lightbox-wrapper");
 const closeBtn = document.querySelector(".lightbox__close-button");
 const nextBtn = document.querySelector(".lightbox__content-next");
 const prevBtn = document.querySelector(".lightbox__content-previous");
@@ -25,6 +24,7 @@ const mediaContainer = document.querySelector(".lightbox__media-display");
 const mediaTitle = document.querySelector(".lightbox__media-title");
 const links = document.getElementsByClassName("media__link");
 
+//initLbox appelée dans displayPortfolio
 function initLightbox() {
 	openLightbox();
 
@@ -38,7 +38,6 @@ function initLightbox() {
 				openLightbox(link);
 			};
 		});
-		
 	};
 	//gestion des événements de Lightbox au clavier
 	document.addEventListener("keydown", function(event) {
@@ -47,17 +46,13 @@ function initLightbox() {
 		};
 		//ici rajouter les évéments pour flèches next / prev
 	});
-
 };
 
 function openLightbox() {
-	//const links = document.getElementsByClassName("media__link");
-
 	//Lancement de la lighbox onclick sur le lien de média
 	for (let link of links) {
 		link.addEventListener("click", event => {
 			event.preventDefault();
-			//lightboxBackgroud.style.display = "block";
 			lightbox.style.display = "block";
 			main.setAttribute("aria-hidden", true);
 			lightbox.setAttribute("aria-hidden", false);
@@ -66,6 +61,7 @@ function openLightbox() {
 		});
 	};	
 };
+
 // Affichage du média contenu dans le lien cliqué dans la Lightbox
 function displayMediaLightbox(link) {
 	mediaContainer.innerHTML = link.firstElementChild.outerHTML;
@@ -76,7 +72,6 @@ function displayMediaLightbox(link) {
 	};
 };
 
-
 //Fermeture de la Lightbox
 function closeLightbox() {
 	lightbox.style.display = "none";
@@ -84,6 +79,8 @@ function closeLightbox() {
 	lightbox.setAttribute("aria-hidden", true);
 	mediaContainer.innerHTML = "";
 }
+
+
 
 /*//Récupération du lien du média avec son ID
 function getUrlMedia(mediaId) {
