54dba0fabb6f4a89c4c49f59b70385cea50bf8fe
ZhannaZucher <jzucher@gmail.com>
2023-02-16 10:52:41 +0100

    add : open close Lbox on keyboard events

--- scripts/utils/lightbox.js
+++ scripts/utils/lightbox.js
@@ -1,63 +1,73 @@
 /*
 TODO list:
 1. fonction initLightbox avec les listeners  pour:
-	ouvrir LBOX (fonction à créer) + gestion des aria
-   fermer LBOX (fonction à créer) + gestion des aria
-	goToNext (fonction à créer)
-	goToPrevious (fonction à créer) 
-	events keybord
-
+	OK! ouvrir LBOX (fonction à créer) + gestion des aria
+   	fermer LBOX (fonction à créer) + gestion des aria
+		goToNext (fonction à créer)
+		goToPrevious (fonction à créer) 
+		events keybord
 2. fonction "ouvrir LBOX" avec
-	Lbox style block + idem pour lBOX bcgrd
-	main : aria hidden=true, LBOX wrapper = false
-	focus sur btn close
-	appel de la fonction setMediaView (soit avec links soit avec id)
-
-3. fonction fermer LBOX:
-	display none + aria + focus
-
+		Lbox style block + idem pour lBOX bcgrd
+		main : aria hidden=true, LBOX wrapper = false
+		focus sur btn close
+		appel de la fonction setMediaView (soit avec links soit avec id)
+3. OK! fonction fermer LBOX:
+		display none + aria + focus
 4. fonction qui gère next/prev slideShow (avec index)
 ...
 */
 //Récupération des éléments DOM
-//const main = document.querySelector("#main"); //a mettre dans display
-const lightboxBackgroud = document.querySelector(".background-lightbox");//a mettre dans display
 const lightbox = document.querySelector(".lightbox-wrapper");//a mettre dans display
 const closeBtn = document.querySelector(".lightbox__close-button");
 const nextBtn = document.querySelector(".lightbox__content-next");
 const prevBtn = document.querySelector(".lightbox__content-previous");
-//const links = document.getElementsByClassName("media__link");
+const mediaContainer = document.querySelector(".lightbox__media-display");
+const mediaTitle = document.querySelector(".lightbox__media-title");
+const links = document.getElementsByClassName("media__link");
 
 function initLightbox() {
 	openLightbox();
-		
 
+	//gestion des événements onclick
+	closeBtn.addEventListener("click", closeLightbox);
+
+	// Lancement de la Lightbox sur l'événement du clavier
+	for (let link of links) {
+		link.addEventListener("keydown", (event) => {
+			if (event.key == "Enter") {
+				openLightbox(link);
+			};
+		});
+		
+	};
+	//gestion des événements de Lightbox au clavier
+	document.addEventListener("keydown", function(event) {
+		if (lightbox.ariaHidden === "false" && event.key === "Escape") {
+			closeLightbox();
+		};
+		//ici rajouter les évéments pour flèches next / prev
+	});
 
 };
 
 function openLightbox() {
-	const links = document.getElementsByClassName("media__link");
+	//const links = document.getElementsByClassName("media__link");
 
 	//Lancement de la lighbox onclick sur le lien de média
 	for (let link of links) {
 		link.addEventListener("click", event => {
 			event.preventDefault();
-			lightboxBackgroud.style.display = "block";
+			//lightboxBackgroud.style.display = "block";
 			lightbox.style.display = "block";
 			main.setAttribute("aria-hidden", true);
 			lightbox.setAttribute("aria-hidden", false);
 			closeBtn.focus();
 			displayMediaLightbox(link);
 		});
-	};
-	
-	
+	};	
 };
 // Affichage du média contenu dans le lien cliqué dans la Lightbox
 function displayMediaLightbox(link) {
-	const mediaContainer = document.querySelector(".lightbox__media-display");
-	const mediaTitle = document.querySelector(".lightbox__media-title");
-
 	mediaContainer.innerHTML = link.firstElementChild.outerHTML;
 	mediaTitle.innerHTML = link.nextElementSibling.firstElementChild.outerHTML;
 	// Dans le cas où le média visé est une vidéo on rajoute les controls pour sa lecture
@@ -65,6 +75,15 @@ function displayMediaLightbox(link) {
 		mediaContainer.firstElementChild.setAttribute("controls", true);
 	};
 };
+
+
+//Fermeture de la Lightbox
+function closeLightbox() {
+	lightbox.style.display = "none";
+	main.setAttribute("aria-hidden", false);
+	lightbox.setAttribute("aria-hidden", true);
+	mediaContainer.innerHTML = "";
+}
 
 /*//Récupération du lien du média avec son ID
 function getUrlMedia(mediaId) {
